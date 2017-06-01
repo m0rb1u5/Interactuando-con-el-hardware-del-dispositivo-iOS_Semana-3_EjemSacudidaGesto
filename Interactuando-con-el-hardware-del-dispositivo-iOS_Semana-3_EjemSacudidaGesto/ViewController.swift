e45376a9ee1990f3ba62d9c2801bcee73fcd1a08
m0rb1u5 <goesrex@gmail.com>
2017-06-01 01:45:26 -0500

    Clase "Detección de "sacudida" como un gesto".

--- Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-3_EjemSacudidaGesto/ViewController.swift
+++ Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-3_EjemSacudidaGesto/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var sacudidaEtiqueta: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            self.sacudidaEtiqueta.text = "Sí!!!"
+        }
+    }
 
+    @IBAction func limpiar() {
+        self.sacudidaEtiqueta.text = ""
+    }
 
 }
 
